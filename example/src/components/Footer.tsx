9f8ea52bfb2258fc8c27c7ab66638cfcbcd7af5f
Tikaram <tikaramac19@gmail.com>
2025-03-13 09:21:53 +0545

    Feat: Example configured

--- example/src/components/Footer.tsx
+++ example/src/components/Footer.tsx
@@ -1,14 +1,13 @@
 import React from "react"
 
 const Footer = () => (
-    <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">
-            &copy; 2019-{(new Date()).getFullYear()}{" "}
-            <a href="https://github.com/puncoz-official">Puncoz Nepal</a>
+    <footer className='my-5 pt-5 text-muted text-center text-small'>
+        <p className='mb-1'>
+            &copy; 2024-{new Date().getFullYear()} <a href='https://vikrant-shrestha.com.np'>Vikrant Shrestha</a>
         </p>
-        <ul className="list-inline">
-            <li className="list-inline-item">
-                <a href="https://github.com/puncoz-official/nepali-datepicker-reactjs">Github</a>
+        <ul className='list-inline'>
+            <li className='list-inline-item'>
+                <a href='https://github.com/vincentvicran/react-nepali-datepicker'>Github</a>
             </li>
         </ul>
     </footer>
